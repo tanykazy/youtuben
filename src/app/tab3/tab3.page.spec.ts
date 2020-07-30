04b81e5b25cba8f36ddbda2998f8a38487d17429
t-cool <hiroki802@hotmail.com>
2020-07-30 16:04:37 +0900

    organized test codes

--- src/app/tab3/tab3.page.spec.ts
+++ src/app/tab3/tab3.page.spec.ts
@@ -5,19 +5,4 @@ import { Tab3Page } from './tab3.page';
 describe('Tab3Page', () => {
   let component: Tab3Page;
   let fixture: ComponentFixture<Tab3Page>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [Tab3Page],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(Tab3Page);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
