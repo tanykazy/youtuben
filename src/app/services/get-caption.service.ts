eed977dbf66f8051c949e84bf93d5845f0905271
tanykazy <tanykazy@gmail.com>
2020-08-20 01:31:26 +0900

    During refactoring

--- src/app/services/get-caption.service.ts
+++ src/app/services/get-caption.service.ts
@@ -1,132 +1,147 @@
 import { Injectable } from '@angular/core';
 
+const youtubeTimedtextUrl = 'https://video.google.com/timedtext';
+
 @Injectable({
   providedIn: 'root',
 })
 export class GetCaptionService {
-  constructor() {}
-  getYouTubeVideoId() {
-    const videoID = window.location.search.split('v=')[1];
-    if (videoID != null) {
-      const ampersandPosition = videoID.indexOf('&');
-      if (ampersandPosition !== -1) {
-        return videoID.substring(0, ampersandPosition);
-      }
-    }
-    return null;
-  }
-  loadYouTubeSubtitles(videoId, options) {
-    options = Object.assign(
-      {
-        baseUrl: 'https://video.google.com/timedtext',
-        languageId: 'en',
-        callbackFn(json) {
-          console.log(json);
-        },
-      },
-      options || {}
-    );
 
-    const decodeHTML = (() => {
-      const el = document.createElement('div');
-      function __decode(str) {
-        if (str && typeof str === 'string') {
-          str = str
-            .replace(/<script[^>]*>([\S\s]*?)<\/script>/gim, '')
-            .replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gim, '');
-          el.innerHTML = str;
-          str = el.textContent;
-          el.textContent = '';
-        }
-        return str;
-      }
-      removeElement(el);
-      return __decode;
-    })();
+  constructor() { }
 
-    function removeElement(el) {
-      // tslint:disable-next-line:no-unused-expression
-      el && el.parentNode && el.parentNode.removeChild(el);
-    }
-
-    function parseTranscriptAsJSON(xml) {
-      return [].slice
-        .call(xml.querySelectorAll('transcript text'))
-        .map((text) => ({
-          start: formatTime(Math.floor(text.getAttribute('start'))),
-          dur: formatTime(Math.floor(text.getAttribute('dur'))),
-          text: decodeHTML(text.textContent).replace(/\s+/g, ' '),
-        }));
-    }
-
-    function formatTime(seconds) {
-      const date = new Date(null);
-      date.setSeconds(seconds);
-      return date.toISOString().substr(11, 8).slice(4, 11);
-      //    return Date.parse(date) * 0.001;
-    }
-
-    const xhr = new XMLHttpRequest();
-    xhr.open(
-      'POST',
-      `${options.baseUrl}?lang=${options.languageId}&v=${videoId}`,
-      true
-    );
-    xhr.responseType = 'document';
-    xhr.onload = function() {
-      if (this.status >= 200 && this.status < 400) {
-        options.callbackFn(parseTranscriptAsJSON(this.response));
-      } else {
-        console.log('Error: ' + this.status);
-      }
-    };
-
-    xhr.onerror = () => {
-      console.log('Error!');
-    };
-
-    xhr.send();
-  }
-
-  loadYoutubeSubTitlesAsync(options: { videoId: string; languageId: string }) {
-    return new Promise((resolve, reject) => {
-      this.loadYouTubeSubtitles(options.videoId, {
-        languageId: options.languageId,
-        callbackFn: (json) => {
-          const result = this.jsonToCsv(json, {
-            includeHeader: false,
-            ignoreKeys: ['dur'],
-            delimiter: '\t',
-          });
-          resolve(result);
-        },
-      });
+  public loadYouTubeSubtitles(requests: SubtitleRequest[]) {
+    const jobs = requests.map((request) => {
+      return xmlHttpRequest(
+        'GET',
+        buildYoutubeUrl(request.videoid, request.languageid),
+        { responseType: 'document' });
     });
-  }
+    // console.log(jobs);
 
-  jsonToCsv(json, options) {
-    options = Object.assign(
-      {
-        includeHeader: true,
-        delimiter: ',',
-        ignoreKeys: [],
-      },
-      options || {}
-    );
-    const keys = Object.keys(json[0]).filter(
-      (key) => options.ignoreKeys.indexOf(key) === -1
-    );
-    const lines = [];
-    if (options.includeHeader) {
-      lines.push(keys.join(options.delimiter));
+    let result = doParallel(jobs);
+
+    function resolveAfter2Seconds(x) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(x);
+        }, 2000);
+      });
     }
-    return lines
-      .concat(
-        json.map((entry) =>
-          keys.map((key) => entry[key]).join(options.delimiter)
-        )
-      )
-      .map((entry) => entry.replace('-', ''))
-      .map((entry) => entry.split('	'));
+
+    async function f1() {
+      var x = await resolveAfter2Seconds(10);
+      console.log(x); // 10
+    }
+    let a = f1();
+    console.log(a);
+
+    return Promise.resolve(result).then((a) => a);
   }
+}
+
+function doParallel(jobs: Promise<any>[]): Promise<any> {
+  return Promise.all(jobs);
+}
+
+
+function buildYoutubeUrl(videoid: string, langid: string): string {
+  return `${youtubeTimedtextUrl}?lang=${langid}&v=${videoid}`;
+}
+
+function xmlHttpRequest(method: string, url: string, options?: {
+  body?: any,
+  async?: boolean,
+  responseType?: XMLHttpRequestResponseType
+}): Promise<any> {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = options.responseType;
+    xhr.open(
+      method,
+      url,
+      typeof options.async === 'undefined' ? true : options.async);
+    xhr.onload = () => {
+      resolve(xhr.response);
+    };
+    xhr.onerror = () => {
+      console.log('** An error occurred during the transaction');
+      reject(xhr.status);
+    };
+    xhr.send(options.body);
+  });
+}
+
+function parseTranscriptAsJSON(xml) {
+  return [].slice
+    .call(xml.querySelectorAll('transcript text'))
+    .map((text) => ({
+      start: formatTime(Math.floor(text.getAttribute('start'))),
+      dur: formatTime(Math.floor(text.getAttribute('dur'))),
+      text: decodeHTML(text.textContent).replace(/\s+/g, ' '),
+    }));
+}
+
+function formatTime(seconds) {
+  const date = new Date(null);
+  date.setSeconds(seconds);
+  return date.toISOString().substr(11, 8).slice(4, 11);
+  //    return Date.parse(date) * 0.001;
+}
+
+function jsonToCsv(json, options) {
+  options = Object.assign(
+    {
+      includeHeader: true,
+      delimiter: ',',
+      ignoreKeys: [],
+    },
+    options || {}
+  );
+  const keys = Object.keys(json[0]).filter(
+    (key) => options.ignoreKeys.indexOf(key) === -1
+  );
+  const lines = [];
+  if (options.includeHeader) {
+    lines.push(keys.join(options.delimiter));
+  }
+  return lines
+    .concat(
+      json.map((entry) =>
+        keys.map((key) => entry[key]).join(options.delimiter)
+      )
+    )
+    .map((entry) => entry.replace('-', ''))
+    .map((entry) => entry.split('	'));
+}
+
+function decodeHTML(str) {
+  const el = document.createElement('div');
+  if (str && typeof str === 'string') {
+    str = str
+      .replace(/<script[^>]*>([\S\s]*?)<\/script>/gim, '')
+      .replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gim, '');
+    el.innerHTML = str;
+    str = el.textContent;
+    el.textContent = '';
+  }
+  if (el && el.parentNode) {
+    el.parentNode.removeChild(el);
+  }
+  return str;
+}
+
+export interface SubtitleRequest {
+  videoid: string;
+  languageid: string;
+}
+
+export interface Captions {
+  captions: Array<Caption>;
+  lang: string;
+}
+
+export interface Caption {
+  start: number;
+  dur: number;
+  text: string;
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
 import { PlyrComponent } from 'ngx-plyr';
-import { GetCaptionService } from '../services/get-caption.service';
+import { GetCaptionService, Captions, Caption, SubtitleRequest } from '../services/get-caption.service';
 
 import { RecordCountService } from '../services/record-count.service';
 
@@ -14,18 +14,20 @@ import { RecordCountService } from '../services/record-count.service';
 export class Tab1Page implements OnInit {
   constructor(
     private getCaptionService: GetCaptionService,
-    private recordCountService: RecordCountService) {
-    }
+    private recordCountService: RecordCountService) { }
   // プロパティ一覧
   title = 'Practice';
 
-  videoBank = ['eIho2S0ZahI', 'R1vskiVDwl4', 'F6ehyV7kqv8', 
-               'O_9TgmIg1nA', 'aH--sKPgbSE', 'A3Yj3q0QRLM'];
+  videoBank = [
+    'eIho2S0ZahI', 'R1vskiVDwl4', 'F6ehyV7kqv8',
+    'O_9TgmIg1nA', 'aH--sKPgbSE', 'A3Yj3q0QRLM'];
 
-  videoId = this.videoBank[ Math.floor( Math.random() * this.videoBank.length)];
+  videoId = this.videoBank[Math.floor(Math.random() * this.videoBank.length)];
 
+  captionInfo: CaptionInfo;
   captions = [];
-  currentCaptions: Array<any>;
+  // captions: any;
+  currentCaptions: Array<Caption>;
   captionIndex = 0;
   captionFlag = true;
   videoSources: Plyr.Source[] = [
@@ -49,28 +51,24 @@ export class Tab1Page implements OnInit {
 
   clickPlay() {
     this.plyr.player.play();
-  } 
-  
-  clickCaption(event) {
-    const seconds =
-      Number(event.split(':')[0] * 60) +
-      Number(event.split(':')[1]) +
-      Number(0.00000000111);
-    this.moveToTime(seconds);
+  }
+
+  public clickCaption(event): void {
+    this.moveToTime(event);
     this.recordCountService.addCount();
     this.recordCountService.saveRecord();
   }
 
-  moveToTime(seconds) {
-    this.plyr.player.currentTime = 0;
+  private moveToTime(seconds: number): void {
     this.plyr.player.currentTime = seconds;
     this.plyr.player.play();
   }
 
-  changeCaption() {
+  public changeCaption(): void {
     this.captionIndex = this.captionIndex + 1;
     this.captionIndex = this.captionIndex % this.captions.length;
     this.currentCaptions = this.captions[this.captionIndex];
+    // this.currentCaptions = this.captionInfo.toggleLang().captions;
   }
 
   checkCaptionFlag() {
@@ -87,19 +85,101 @@ export class Tab1Page implements OnInit {
     this.plyr.player.speed = 0.5;
   }
 
-  async ngOnInit() {
+  ngOnInit() {
     this.recordCountService.loadRecord();
-    this.captions = await Promise.all([
-      this.getCaptionService.loadYoutubeSubTitlesAsync({
-        languageId: 'en',
-        videoId: this.videoId,
-      }),
-      this.getCaptionService.loadYoutubeSubTitlesAsync({
-        languageId: 'ja',
-        videoId: this.videoId,
-      }),
-    ]);
+    // this.captions = await Promise.all([
+    //   this.getCaptionService.loadYoutubeSubTitlesAsync({
+    //     languageId: 'en',
+    //     videoId: this.videoId,
+    //   }),
+    //   this.getCaptionService.loadYoutubeSubTitlesAsync({
+    //     languageId: 'ja',
+    //     videoId: this.videoId,
+    //   }),
+    // ]);
+
+    const test = this.getCaptionService.loadYouTubeSubtitles([
+      { videoid: this.videoId, languageid: 'en' },
+      { videoid: this.videoId, languageid: 'ja' }]);
+    console.log('-----------');
+    console.log(test);
+
+    // const cap = {
+    //   en: this.getCaptionService.loadYoutubeSubTitlesAsync({
+    //     languageId: 'en',
+    //     videoId: this.videoId,
+    //   }),
+    //   ja: this.getCaptionService.loadYoutubeSubTitlesAsync({
+    //     languageId: 'ja',
+    //     videoId: this.videoId,
+    //   })
+    // };
+    // console.log(cap);
+
+    // console.log(this.captions);
+
+    // for (const caption of this.captions) {
+    //   for (const caps of caption) {
+    //     caps.start = timeString2seconds(caps.start);
+    //   }
+    // }
+    // const cap = [{
+    //   captions: this.captions[0],
+    //   lang: 'en'
+    // }, {
+    //   captions: this.captions[0],
+    //   lang: 'ja'
+    // }
+    // ];
+    // console.log(cap);
+
     this.captionIndex = 0;
     this.currentCaptions = this.captions[this.captionIndex];
+
+    // console.log(this.captions);
+    // this.captionInfo = new CaptionInfo(cap);
+  }
+}
+
+function timeString2seconds(hhmmss: string): number {
+  const ssmmhh: Array<string> = hhmmss.split(':').reverse();
+  const base = 60;
+  let n = 0;
+  let result = 0;
+  result += parseInt(ssmmhh[n], 10);
+  n += 1;
+  while (n < ssmmhh.length) {
+    result += parseInt(ssmmhh[n], 10) * (base * n);
+    n += 1;
+  }
+  return result;
+}
+
+class CaptionInfo {
+  private captionsList: Array<Captions>;
+  private captionsIndex: number;
+  public current: Captions;
+
+  constructor(captionsList: Array<Captions>) {
+    this.captionsIndex = 0;
+    this.captionsList = Array.from(captionsList);
+    this.current = this.captionsList[this.captionsIndex];
+  }
+
+  public toggleLang(lang?: string): Captions {
+    let captions: Captions;
+    if (!lang) {
+      this.captionsIndex = this.captionsIndex + 1;
+      this.captionsIndex = this.captionsIndex % this.captionsList.length;
+      captions = this.captionsList[this.captionsIndex];
+    } else {
+      captions = this.captionsList.find(
+        (element: Captions) => element.lang === lang);
+      if (!captions) {
+        throw new Error(`${lang} is not found in caption list.`);
+      }
+    }
+    this.current = captions;
+    return this.current;
   }
 }
